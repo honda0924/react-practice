52e11d3115b738e392c087294567bb3eb5d41a99
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2020-11-28 20:43:25 +0900

    why jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React,{ Component } from 'react';
-
-function App() {
-  return (
-    <div>
-        <h1>
-          Hello,World!
-        </h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <h1>Hello,World!</h1>
+    );
+  }
 }
 
 export default App;
