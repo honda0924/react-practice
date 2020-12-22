3d1d2b52adb8ca2a96dbd5c18eee28a0c8ab66a4
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2020-12-22 11:07:05 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+    tyoe: 'INCREMENT'
+})
+export const decrement = () => ({
+    tyoe: 'DECREMENT'
+})
